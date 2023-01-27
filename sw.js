78cf9086561eef93171c4809e03e361553f7e16b
CjSever <Pin.Evdokimov@yandex.ru>
2023-01-27 16:40:30 +0400

    test Until

--- sw.js
+++ sw.js
@@ -1,10 +1,44 @@
-self.addEventListener('install', (event) => {
-    console.log('Установлен');
-});
+// Внимание: ваши данные могут отличаться!
+// const CACHE_NAME = 'your-super-game-name-v1.0.3';
+// const cacheList = [
+//     'https://yandex.ru/games/sdk/v2',
+//     'index.html',
+//     'example.js'
+// ];
+
+// self.addEventListener('install', (event) => {
+//     console.log('Установлен');
+// });
 
 self.addEventListener('activate', (event) => {
     console.log('Активирован');
 });
+
+self.addEventListener('install', (event) => {
+    
+    console.log('Установлен');
+
+    event.waitUntil(
+      addResourcesToCache([
+        './',
+        './index.html',
+        './Style/StyleAudioPlayer.css',
+        './app.js',
+
+        './Control',
+        './Control/CheckBrowser.js',
+        './Control/ControlPlayer.js',
+        './Control/ReplaceBlockAudioPlayer.js',
+        
+        './img',
+        './img/vol_level_1.png',
+        './img/vol_level_2.png',
+        './img/vol_level_3.png',
+        './img/vol_level_mute.png'
+      ])
+    );
+  });
+
 
 self.addEventListener('fetch', (event) => {
     console.log('Происходит запрос на сервер');
